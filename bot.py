23b60c162d20d0077e7ed8dad0817eb48570f14b
JorgeAmVF <javf.pub@icloud.com>
2019-03-24 23:55:32 -0300

    Bug fix and output formatting

--- bot.py
+++ bot.py
@@ -11,101 +11,111 @@ def bot_login():
                 password = config.password,
                 client_id = config.client_id,
                 client_secret = config.client_secret,
-                user_agent = config.user_agent)
+                user_agent = "reddit:r/EmPortugues:vAlpha (by u/BoEmPortugues)")
     print ("Conectado.")
 
     return r
 
 def run_bot(r, submissions_replied_to):
-    print ("Varrendo as 25 postagens mais recentes...")
+    print ("Verificiando as 1000 postagens mais recentes em busca de links...")
 
-    for submission in r.subreddit('test').new(limit=25):
+    for submission in r.subreddit('emportugues').new(limit=1000):
         if submission.is_self is False and submission.url not in submissions_replied_to:
             sub_url = submission.url
-            sub = sub_url.replace("https://www.reddit.com/r/", "")
-        
-            print ("Encontrada postagem com o seguinte link para subreddit: " + submission.url)
-            print ("Comentando na postagem sobre o subreddit " + "r/" + sub + "...")
+            sub_name = sub_url.replace("https://www.reddit.com/r/", "")
+            sub = r.subreddit(sub_name).display_name
+            
+            print ("Encontrada postagem com o seguinte link para subreddit: " + submission.url + ".")
             
             submissions_replied_to.append(submission.url)
             
+            print ("Coletando dados sobre o subreddit " + r.subreddit(sub).display_name_prefixed + "...")
+       
             #nome do subreddit
             subr = r.subreddit(sub).display_name_prefixed
-            #data de crialção
-            crea = int(r.subreddit(sub).created_utc)
             #descrição completa
-            desc = ""
-            if desc != "": "\"" + r.subreddit(sub).public_description + "\""
-            elif desc == "": desc = "?"
+            public_description = r.subreddit(sub).public_description
+            if public_description == "": desc = "?"
+            if public_description != "": desc = "\"" + r.subreddit(sub).public_description.replace("\n", " ") + "\""            
+            #data de crialção
+            created_utc = int(r.subreddit(sub).created_utc)
+            crea = "criado em " + datetime.utcfromtimestamp(created_utc).strftime("%d/%m/%Y às %H:%M:%S")
             #quantidade de subscrições
-            subs = r.subreddit(sub).subscribers
-            #tipo de postagem
-            subm = r.subreddit(sub).submission_type
-            if subm == "any": subm = "links e textos"
-            if subm == "link": subm = "apenas links"
-            if subm == "text": subm = "apenas textos"
-            #flair para postagens
-            lfla = r.subreddit(sub).can_assign_link_flair
-            if lfla == True: lfla = "sim" 
-            elif lfla == False: lfla = "não"             
-            #flair para usuários
-            ufla = r.subreddit(sub).can_assign_user_flair
-            if ufla == True: ufla = "sim" 
-            elif ufla == False: ufla = "não"            
-            #permite imagens
-            imag = r.subreddit(sub).allow_images
-            if imag == True: imag = "sim" 
-            elif imag == False: imag = "não"            
-            #permite gifs
-            gifs = r.subreddit(sub).allow_videogifs
-            if gifs == True: gifs = "sim" 
-            elif gifs == False: gifs = "não"
-            #permite vídeos
-            vids = r.subreddit(sub).allow_videos
-            if vids == True: vids = "sim" 
-            elif vids == False: vids = "não"
-            #conteúdo adulto
-            nsfw = r.subreddit(sub).over18
-            if nsfw == False: nsfw = "não" 
-            elif nsfw == True: nsfw = "sim"
-            #em quarentena
-            quar = r.subreddit(sub).quarantine
-            if quar == False: quar = "não" 
-            elif quar == True: quar = "sim"
+            subscribers = str(r.subreddit(sub).subscribers)
+            if subscribers == 0: subs = "nenhum usuário subscrito"
+            if subscribers == 1: subs = "só um usuário subscrito"
+            else: subs = subscribers + " usuários subscritos"
             #wiki disponível
-            wiki = r.subreddit(sub).wiki_enabled
-            if wiki == True:  wiki = "<a href=" + submission.url + "/wiki/index>sim</a>" 
-            elif wiki != True: wiki = "não"  
+            wiki_enabled = r.subreddit(sub).wiki_enabled
+            if wiki_enabled == True:  wiki = "[wiki disponível](" + submission.url + "/wiki/index)" 
+            else: wiki = "wiki indisponível"
             #tipo de acesso
-            type = r.subreddit(sub).subreddit_type
-            if type == "public": type = "público" 
-            if type == "restricted": type = "restrito"
-            if type == "private": type = "privado"
-            if type == "gold_restricted": type = "premium"
-            if type == "archived": type = "arquivado"
-
+            subreddit_type = r.subreddit(sub).subreddit_type
+            if subreddit_type == "public": type = "conteúdo público" 
+            if subreddit_type == "restricted": type = "conteúdo restrito"
+            if subreddit_type == "private": type = "conteúdo privado"
+            if subreddit_type == "gold_restricted": type = "conteúdo premium"
+            if subreddit_type == "archived": type = "conteúdo arquivado"            
+            #tipo de postagem
+            submission_type = r.subreddit(sub).submission_type
+            if submission_type == "any": subm = "permite textos e hiperligações"
+            if submission_type == "link": subm = "permite apenas hiperligações"
+            if submission_type == "text": subm = "permite apenas textos"
+            #permissão de imagens, allow_videogifs e vídeos
+            allow_images = r.subreddit(sub).allow_images
+            allow_videogifs = r.subreddit(sub).allow_videogifs
+            allow_videos = r.subreddit(sub).allow_videos
+            if allow_images == True and allow_videogifs == True and allow_videos == True: mult = "aceita imagens, GIFs e vídeos" 
+            if allow_images == True and allow_videogifs == True and allow_videos == False: mult = "aceita somente imagens e GIFs" 
+            if allow_images == True and allow_videogifs == False and allow_videos == True: mult = "aceita somente imagens e vídeos" 
+            if allow_images == True and allow_videogifs == False and allow_videos == False: mult = "aceita exclusivamente imagens" 
+            if allow_images == False and allow_videogifs == True and allow_videos == True: mult = "aceita somente GIFs e vídeos" 
+            if allow_images == False and allow_videogifs == True and allow_videos == False: mult = "aceita exclusivamente GIFs" 
+            if allow_images == False and allow_videogifs == False and allow_videos == True: mult = "aceita exclusivamente vídeos" 
+            if allow_images == False and allow_videogifs == False and allow_videos == False: mult = "não aceita imagens, GIFs nem vídeos" 
+            #flair para postagens para usuários
+            ufla = r.subreddit(sub).can_assign_user_flair
+            lfla = r.subreddit(sub).can_assign_link_flair
+            if lfla == True and ufla == True: flair = "disponibiliza flairs para usários e postagens" 
+            if lfla == True and ufla == False: flair = "disponibiliza flairs somente para postagens" 
+            if lfla == False and ufla == True: flair = "disponibiliza flairs somente para usuários"    
+            if lfla == False and ufla == False: flair = "não disponibilzia flairs para postagens nem usuários"           
+            #conteúdo adulto
+            over18 = r.subreddit(sub).over18
+            if over18 == False: nsfw = "seguro para o trabalho" 
+            else: nsfw = "não seguro para o trabalho "
+            #em quarentena
+            quarantine = r.subreddit(sub).quarantine
+            if quarantine == False: quar = "em estado normal"
+            else: quar = "em estado de quarentena"
+            
+            print ("Preparando a tabela para comentar na postagem...")
+                        
             submission.reply(
-                "#" + subr + "\n" +
-                "---" + "\n \n" +
-                "**Descrição completa:** " +  desc + "\n \n" +
-                "**Data de criação:** " + (datetime.utcfromtimestamp(crea).strftime("%d/%m/%Y às %H:%M:%S")) + "\n \n" +
-                "**Quantidade de subcrições:** " + str(subs) + "\n \n" +
-                "**Tipo de postagem:** " + subm  + "\n \n" +
-                "**Permite imagens: " + imag + "\n \n" +
-                "**Permite vídeos: " + vids + "\n \n" +                
-                "**Em quarentena:** " + quar + "\n \n" +
-                "---" + "\n \n" +
-                "^Eu ^sou ^um ^bô, ^blipe, ^blupe. ^| ^r/EmPortugues ^| ^[Mensagem](http://reddit.com/message/compose/?to=RoboEmPortugues&subject=Robô%%20Report) ^| ^Código")
+            subr + "\n" +
+            ":-: | \n" +
+            desc + " | \n" +
+            crea + " | \n" +
+            subs + " | \n" +
+            wiki  + " | \n" +
+            type  + " | \n" +
+            subm  + " | \n" +          
+            mult + " | \n" + 
+            flair + " | \n" + 
+            nsfw + " | \n" +             
+            quar + " | \n \n" +
+            "^(Eu sou um bô, blipe, blupe. | [Sub](https://www.reddit.com/r/EmPortugues) | [Site](https://emportugues.org/) | [Aplicativo](https://play.google.com/store/apps/details?id=org.emportugues.aplicativo) | [Organização](https://github.com/subreddit-emportugues) | [Mensagem](http://reddit.com/message/compose/?to=BoEmPortugues&subject=Eu não sou um bô.))")
+            
+            print ("Comentário postado com sucesso!")
             
             with open ("submissions_replied_to.txt", "a") as f:
                 f.write(submission.url + "\n")
-
-    print ("Varredura completa!")
-
+                print ("Endereço adicionado à listagem.")
+    
     print (submissions_replied_to)
 
-    print ("Aguardando 60 segundos para recomeçar...")
-    time.sleep(60)
+    print ("Aguardando 10 minutos para recomeçar...")
+    time.sleep(600)
 
 def get_saved_comments():
     if not os.path.isfile("submissions_replied_to.txt"):
@@ -129,7 +139,7 @@ def is_summon_chain(post):
   if not post.is_root:
     parent_comment_id = post.parent_id
     parent_comment = r.get_info(thing_id=parent_comment_id)
-    if parent_comment.author != None and str(parent_comment.author.name) == 'config.username':
+    if parent_comment.author != None and str(parent_comment.author.name) == config.username:
       return True
     else:
       return False
@@ -139,7 +149,8 @@ def is_summon_chain(post):
 def comment_limit_reached(post):
   global submissioncount
   count_of_this = int(float(submissioncount[str(post.submission.id)]))
-  if count_of_this > 10:
+  if count_of_this > 1:
+    print ("A tarefa já foi realizada nesta postagem anteriormente.")
     return True
   else:
     return False
@@ -154,7 +165,7 @@ def is_already_done(post):
     pass
   if numofr != 0:
     for repl in post.replies:
-      if repl.author != None and repl.author.name == 'config.username':
+      if repl.author != None and repl.author.name == config.username:
         done = True
         continue
   if done:
