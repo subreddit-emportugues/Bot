8b6d3e12a16f6f78be5540eb64a58ad01e021cf1
JorgeAmVF <javf.pub@icloud.com>
2019-04-06 16:20:44 -0300

    Terminate successfully and remove unused code

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import praw
 import config
-import time
 import os
-import collections, praw
 from datetime import datetime
+import sys  
 
 def bot_login():
     print ("Conectando...")
@@ -19,7 +18,7 @@ def bot_login():
 def run_bot(r, submissions_replied_to):
     print ("Verificiando as 1000 postagens mais recentes em busca de links...")
 
-    for submission in r.subreddit('emportugues').new(limit=1000):
+    for submission in r.subreddit('EmPortugues').new(limit=1000):
         if submission.is_self is False and submission.url not in submissions_replied_to:
             submission_url = submission.url
             if submission_url.endswith("/"): sub_url = submission.url[:-1]
@@ -52,9 +51,9 @@ def run_bot(r, submissions_replied_to):
             if mod_list > 1: mods = str(mod_list) + " moderadores"
             #quantidade de subscrições
             subscribers = str(r.subreddit(sub).subscribers)
-            if subscribers == 0: subs = "nenhum usuário subscrito"
-            if subscribers == 1: subs = "só um usuário subscrito"
-            else: subs = subscribers + " usuários subscritos"
+            if subscribers == 0: subs = "nenhum membro na comunidade"
+            if subscribers == 1: subs = "só um membro na comunidade"
+            else: subs = subscribers + " membros na comunidade"
             #wiki disponível
             wiki_enabled = r.subreddit(sub).wiki_enabled
             if wiki_enabled == True:  wiki = "[wiki disponível](" + sub_url + "/wiki/index)" 
@@ -93,7 +92,7 @@ def run_bot(r, submissions_replied_to):
             #conteúdo adulto
             over18 = r.subreddit(sub).over18
             if over18 == False: nsfw = "seguro para o trabalho" 
-            else: nsfw = "não seguro para o trabalho "
+            else: nsfw = "não seguro para o trabalho"
             #em quarentena
             quarantine = r.subreddit(sub).quarantine
             if quarantine == False: quar = "em estado normal"
@@ -122,7 +121,7 @@ def run_bot(r, submissions_replied_to):
             
             comment.mod.distinguish(how='yes', sticky=True)
             
-            print ("Comentário postado e fixado com sucesso!")
+            print ("Comentário postado e fixado!")
             
             with open ("submissions_replied_to.txt", "a") as f:
                 f.write(submission.url + "\n")
@@ -130,8 +129,8 @@ def run_bot(r, submissions_replied_to):
     
     print (submissions_replied_to)
 
-    print ("Aguardando 10 minutos para recomeçar...")
-    time.sleep(600)
+    print ("Procedimento terminado com sucesso!")
+    sys.exit(0)
 
 def get_saved_comments():
     if not os.path.isfile("submissions_replied_to.txt"):
@@ -150,56 +149,3 @@ print (submissions_replied_to)
 
 while True:
     run_bot(r, submissions_replied_to)
-    
-def is_summon_chain(post):
-  if not post.is_root:
-    parent_comment_id = post.parent_id
-    parent_comment = r.get_info(thing_id=parent_comment_id)
-    if parent_comment.author != None and str(parent_comment.author.name) == config.username:
-      return True
-    else:
-      return False
-  else:
-    return False
-  
-def comment_limit_reached(post):
-  global submissioncount
-  count_of_this = int(float(submissioncount[str(post.submission.id)]))
-  if count_of_this > 1:
-    print ("A tarefa já foi realizada nesta postagem anteriormente.")
-    return True
-  else:
-    return False
-  
-def is_already_done(post):
-  done = False
-  numofr = 0
-  try:
-    repliesarray = post.replies
-    numofr = len(list(repliesarray))
-  except:
-    pass
-  if numofr != 0:
-    for repl in post.replies:
-      if repl.author != None and repl.author.name == config.username:
-        done = True
-        continue
-  if done:
-    return True
-  else:
-    return False
-
-def post_reply(reply,post):
-  global submissioncount
-  try:
-    a = post.reply(reply)
-    submissioncount[str(post.submission.id)]+=1
-    return True
-  except Exception as e:
-    warn("A fesposta falhou: %s @ %s"%(e,post.subreddit))
-    if str(e) == "Erro 403: Proibido":
-      print ("Fui banido em " + "r/" + post.subreddit + ".")
-      save_changing_variables()
-    return False
-
-submissioncount = collections.Counter()
